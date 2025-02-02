9a00512c61ff482c5d47b05505f97b01b7e256ac
karyanayandi <xkaryanayandi@gmail.com>
2025-02-02 13:48:49 +0700

    feat: update theme to use Nord Aurora palette
    - Removed Onedark theme references from README.md
    - Updated preview image URL in README.md
    - Changed theme colors in lib/themes.sh to Nord Aurora palette
    - Commented out old theme configurations in lib/themes.sh

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Aurora TMUX Theme
 
-Just a simple tmux theme using Onedark and Nord Aurora color pallete.
+Just a simple tmux theme using Nord Aurora color pallete.
 
 ## Preview
 
-![preview](https://raw.githubusercontent.com/karyanayandi/nord-tmux/main/preview.png) <br />
+![preview](https://raw.githubusercontent.com/aurora-theme/tmux/main/preview.png) <br />
 
 ## Installation
 
 In your `tmux.conf`:
 
 ```bash
-set -g @plugin "karyanayandi/aurora.tmux"
+set -g @plugin "aurora-theme/tmux"
 ```
--- lib/themes.sh
+++ lib/themes.sh
@@ -3,73 +3,87 @@
 SELECTED_THEME="$(tmux show-option -gv @aurora)"
 
 case $SELECTED_THEME in
-"onedark")
+"aurora")
   declare -A THEME=(
-    ["background"]="#282c34"
-    ["foreground"]="#c8ccd4"
-    ["highlight"]="#3e4451"
-    ["active"]="#353b45"
-    ["blue"]="#61afef"
-    ["cyan"]="#56b6c2"
-    ["green"]="#98c379"
-    ["red"]="#be5046"
-    ["yellow"]="#e5c07b"
+    ["background"]="#2e3440"
+    ["foreground"]="#d8dee9"
+    ["highlight"]="#3b4252"
+    ["active"]="#3b4252"
+    ["blue"]="#81a1c1"
+    ["cyan"]="#5e81ac"
+    ["green"]="#a3be8c"
+    ["red"]="#bf616a"
+    ["yellow"]="#ebcb8b"
   )
   ;;
 
-"onenord")
-  declare -A THEME=(
-    ["background"]="#282c34"
-    ["foreground"]="#c8ccd4"
-    ["highlight"]="#3e4451"
-    ["active"]="#353b45"
-    ["blue"]="#81A1C1"
-    ["cyan"]="#88C0D0"
-    ["green"]="#A3BE8C"
-    ["red"]="#D57780"
-    ["yellow"]="#EBCB8B"
-  )
-  ;;
-
-"nord-dark")
-  declare -A THEME=(
-    ["background"]="#2E3440"
-    ["foreground"]="#C8D0E0"
-    ["highlight"]="#4C566A"
-    ["active"]="#3B4252"
-    ["blue"]="#81A1C1"
-    ["cyan"]="#88C0D0"
-    ["green"]="#A3BE8C"
-    ["red"]="#D57780"
-    ["yellow"]="#EBCB8B"
-  )
-  ;;
-
-"nord-light")
-  declare -A THEME=(
-    ["background"]="#EFF0F2"
-    ["foreground"]="#4C566A"
-    ["highlight"]="#D8DEE9"
-    ["active"]="#2E3440"
-    ["blue"]="#3879C5"
-    ["cyan"]="#3EA1AD"
-    ["green"]="#48A53D"
-    ["red"]="#CB4F53"
-    ["yellow"]="#BA793E"
-  )
-  ;;
+# "onedark")
+#   declare -A THEME=(
+#     ["background"]="#282c34"
+#     ["foreground"]="#c8ccd4"
+#     ["highlight"]="#3e4451"
+#     ["active"]="#353b45"
+#     ["blue"]="#61afef"
+#     ["cyan"]="#56b6c2"
+#     ["green"]="#98c379"
+#     ["red"]="#be5046"
+#     ["yellow"]="#e5c07b"
+#   )
+#   ;;
+#
+# "onenord")
+#   declare -A THEME=(
+#     ["background"]="#282c34"
+#     ["foreground"]="#c8ccd4"
+#     ["highlight"]="#3e4451"
+#     ["active"]="#353b45"
+#     ["blue"]="#81A1C1"
+#     ["cyan"]="#88C0D0"
+#     ["green"]="#A3BE8C"
+#     ["red"]="#D57780"
+#     ["yellow"]="#EBCB8B"
+#   )
+#   ;;
+#
+# "nord-dark")
+#   declare -A THEME=(
+#     ["background"]="#2E3440"
+#     ["foreground"]="#C8D0E0"
+#     ["highlight"]="#4C566A"
+#     ["active"]="#3B4252"
+#     ["blue"]="#81A1C1"
+#     ["cyan"]="#88C0D0"
+#     ["green"]="#A3BE8C"
+#     ["red"]="#D57780"
+#     ["yellow"]="#EBCB8B"
+#   )
+#   ;;
+#
+# "nord-light")
+#   declare -A THEME=(
+#     ["background"]="#EFF0F2"
+#     ["foreground"]="#4C566A"
+#     ["highlight"]="#D8DEE9"
+#     ["active"]="#2E3440"
+#     ["blue"]="#3879C5"
+#     ["cyan"]="#3EA1AD"
+#     ["green"]="#48A53D"
+#     ["red"]="#CB4F53"
+#     ["yellow"]="#BA793E"
+#   )
+#   ;;
 
 *)
   declare -A THEME=(
-    ["background"]="#282c34"
-    ["foreground"]="#c8ccd4"
-    ["highlight"]="#3e4451"
-    ["active"]="#353b45"
-    ["blue"]="#81A1C1"
-    ["cyan"]="#88C0D0"
-    ["green"]="#A3BE8C"
-    ["red"]="#D57780"
-    ["yellow"]="#EBCB8B"
+    ["background"]="#2e3440"
+    ["foreground"]="#d8dee9"
+    ["highlight"]="#3b4252"
+    ["active"]="#3b4252"
+    ["blue"]="#81a1c1"
+    ["cyan"]="#5e81ac"
+    ["green"]="#a3be8c"
+    ["red"]="#bf616a"
+    ["yellow"]="#ebcb8b"
   )
   ;;
 esac
